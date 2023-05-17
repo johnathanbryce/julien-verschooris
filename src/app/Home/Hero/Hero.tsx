172a31cf89462940b2f30b41183ee31a7fa2cd87
johnathanbryce <johnathanbryce@rompathlete.com>
2023-05-17 12:10:56 -0700

    finalized hero page, todo: responsive design

--- src/app/Home/Hero/Hero.tsx
+++ src/app/Home/Hero/Hero.tsx
@@ -1,35 +1,57 @@
+'use client'
+import { useState } from 'react';
 import styles from './Hero.module.css'
 import Layout from '@/components/Layout/Layout'
 // internal components
 import SocialMediaIcon from '@/components/SocialMediaIcon/SocialMediaIcon';
 import Button from '@/components/Button/Button';
+import HeroSquare from './HeroSquare/HeroSquare';
 // external libraries
-import {FaFacebookF} from 'react-icons/fa'
-import {BsTwitter} from 'react-icons/bs'
-
+import {SiImdb} from 'react-icons/si'
+import {TbBrandSpotify} from 'react-icons/tb'
+import {FaInstagram} from 'react-icons/fa'
+import {BsPlayFill, BsPauseFill} from 'react-icons/bs'
 
 function Hero() {
+  const [isMuted, setIsMuted] = useState(true);
+  const [isPaused, setIsPaused] = useState(false);
+
+  const toggleAudio = () => {
+    setIsMuted((prevIsMuted) => !prevIsMuted);
+    setIsPaused((prevIsPaused) => !prevIsPaused);
+  };
+
   return (
       <section className={styles.hero}>
-        <video className={styles.background_clip} src="/backgroundClip.mp4" autoPlay loop muted></video>
+        <video className={styles.background_clip} src="/backgroundClip.mp4" autoPlay loop muted={isMuted} />  
+
         <div className={styles.overlay}></div>
         <Layout>
+          <button onClick={toggleAudio}>
+            { isPaused ? <BsPauseFill className={styles.audio_button}/> : <BsPlayFill className={styles.audio_button}/>}       
+          </button>
           <div className={styles.content_container}>
             <div className={styles.intro_content_wrapper}>
               <h1><span className={styles.firstname}>JULIEN</span> VERSCHOORIS </h1>
               <p> A multi award-winning, CASMA-nominated composer, scoring features, and short films as well as commercial music for trailers and branding placements.</p>
-              <Button label={'Learn More'} isTransparent={true} />
+              <div className={styles.btn_container}><Button label={'Learn More'} isTransparent={true} /></div>
             </div>
           </div>
 
-          <div className={styles.icons_container}>
-              <SocialMediaIcon icon={<FaFacebookF />} src='https://www.facebook.com/' />
-              <SocialMediaIcon icon={<BsTwitter />} src='https://www.twitter.com/'/>       
+          <div className={styles.bottom_container}>
+            <div className={styles.icons_container}>
+              <SocialMediaIcon icon={<SiImdb />} src='https://www.imdb.com/name/nm8653985/' />
+              <SocialMediaIcon icon={<TbBrandSpotify />} src='https://open.spotify.com/artist/0vJxBMqJnP2vbqFAxUWR9t?si=UDb8Xds6RReXtgMMnZ0jmw'/>   
+              <SocialMediaIcon icon={<FaInstagram />} src='https://www.instagram.com/julienverschooris//'/> 
+            </div>
+            <div className={styles.squares_container}>
+              <HeroSquare label="VIEW WORK" src=''/>
+              <HeroSquare label="LISTEN TO MUSIC" src='' />
+              <HeroSquare label="LEARN MORE" src=''/>
+            </div>      
           </div>
 
-          <div>
-            
-          </div>
+
 
         </Layout>
       </section>
--- src/app/Home/SelectedMediaClips/SelectedMediaClips.tsx
+++ src/app/Home/SelectedMediaClips/SelectedMediaClips.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function SelectedMediaClips() {
   return (
-    <div>SelectedMediaClips</div>
+    <div>SelectedMediaClips
+
+    </div>
   )
 }
 
