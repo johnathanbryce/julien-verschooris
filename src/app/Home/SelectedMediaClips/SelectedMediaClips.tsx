ab7771aff8a0d75187e0cfeb2aba7550bc39c3e4
johnathanbryce <johnathanbryce@rompathlete.com>
2023-05-18 12:20:20 -0700

    finalized featuredWork section + small updates to mainbioimage width

--- src/app/Home/SelectedMediaClips/SelectedMediaClips.tsx
+++ src/app/Home/SelectedMediaClips/SelectedMediaClips.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import styles from './SelectedMediaClips.module.css'
 
 function SelectedMediaClips() {
   return (
-    <div>SelectedMediaClips
+    <section className={styles.selected_media}>
+      SelectedMediaClips
 
-    </div>
+    </section>
   )
 }
 
